0162941abc6c9ce328db2a76e0ea465ac582e5ba
Alexander-Nazarov <Alexnazavr@yandex.ru>
2018-10-13 16:59:50 +0300

    Update log_parse.py

--- log_parse.py
+++ log_parse.py
@@ -22,15 +22,15 @@ def get_data(log):
     url = url[0: pos]
     return url, request_time
 
-def Is_it_file(log):
-    a, b = get_data(get_logs(log))
-    start = a.rfind('.')
-    postfix = a[start+1:]
+def is_it_file(log):
+    url, time = get_data(get_logs(log))
+    start = utl.rfind('.')
+    postfix = url[start+1:]
     if postfix.isalpha() and 3 <= len(postfix) <= 4:
             return False
     return True
 
-def Request_type(log):
+def request_Type(log):
     start = log.find('\"')
     end = log.find(' ', start+1)
     Type = log[start+1: end]
@@ -45,7 +45,7 @@ def del_www(log):
         log = deepcopy(new_log + new_log1)
     return log
 
-def Time(log):
+def what_time(log):
     return  time.strptime(log[:22], "[%d/%b/%Y %H:%M:%S]")
 
 def response_time(log):
@@ -56,14 +56,13 @@ def response_time(log):
 def average_time(log, LOGS):
     sum_time = 0
     devicer = 0
-    a, b = get_data(get_logs(log))
+    url_log, time_log = get_data(get_logs(log))
     for line in LOGS:
-        c, d = get_data(get_logs(line))
-        if a == c:
-            sum_time += int(d)
+        url_line, time_line = get_data(get_logs(line))
+        if url_log == url_line:
+            sum_time += int(time_line)
             devicer += 1
     return sum_time // devicer
-
 
 def parse(
     ignore_files=False,
@@ -74,7 +73,6 @@ def parse(
     ignore_www=False,
     slow_queries=False
 ):
-
     '''Формирование начального списка логов'''
 
     LOGS=[]
@@ -82,16 +80,13 @@ def parse(
     URLs = []
     Top_urls = []
     f = open('C:\\Users\\User\\Desktop\\Python\\log.log.txt')
-    for i in f:
-        if get_logs(i) == None:
-            continue
-        LOGS.append(get_logs(i))
+    LOGS = [get_logs(i) for i in f if get_logs(i) != None]
 
     '''Игнорирование файлов'''
 
     if ignore_files:
         for i in LOGS:
-            if Is_it_file(i):
+            if is_it_file(i):
                 buffer.append(i)
         LOGS = deepcopy(buffer)
         buffer.clear()
@@ -112,7 +107,7 @@ def parse(
 
     if start_at != None:
         for i in LOGS:
-            if Time(start_at) > Time(i):
+            if what_time(start_at) > what_time(i):
                 continue
             else:
                 buffer.append(i)
@@ -122,7 +117,7 @@ def parse(
 
     if stop_at != None:
         for i in LOGS:
-            if Time(stop_at) < Time(i):
+            if what_time(stop_at) < what_time(i):
                 continue
             else:
                 buffer.append(i)
@@ -133,7 +128,7 @@ def parse(
 
     if request_type != None:
         for i in LOGS:
-            if Request_type(i) == request_type:
+            if request_Type(i) == request_type:
                 buffer.append(i)
         LOGS = deepcopy(buffer)
         buffer.clear()
@@ -161,27 +156,21 @@ def parse(
 
     '''Возвращает среднее значение, потраченное на топ 5
        самых медленных запросов к серверу
-       (сортирует по убыванию среднего времени) '''
-
-    if slow_queries == True:
-        slow_quer = [0] * 5
-        slow_logs = [0] * 5
-        slow = []
-        h = 0
-        buffer = deepcopy(LOGS)
-        for i in range(5):
-            for j in range(1, len(buffer)-h):
-                if int(slow_quer[i]) < int(response_time(buffer[j])):
-                    slow_quer[i] = int(response_time(buffer[j]))
-                    h = j
-                    slow_logs[i] = buffer[j]
-            buffer.pop(h)
-
-        for i in range(5):
-            slow.append(average_time(slow_logs[i], LOGS))
-        slow.sort()
-        slow.reverse()
-        return slow
+    '''
+    if slow_queries:
+        dict = {}
+        for i in LOGS:
+            url, request_time = get_data(i)
+            request_time = request_time
+            if url in dict:
+                dict[url][0] += 1
+                dict[url][1] += int(request_time)
+            else:
+                dict[url] = [1, int(request_time)]
+        list_of_several_time = [int(value[1]) // int(value[0]) for key, value in dict.items()]
+        list_of_several_time.sort()
+        list_of_several_time.reverse()
+        return list_of_several_time[:5]
     else:
         return Top_urls
 
